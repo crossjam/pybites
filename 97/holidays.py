f2375bb8eb552a595adeb4b4e7360883e3a00792
Brian Dennis <bmd+github@crossjam.net>
2021-09-03 18:38:22 -0400

    Initial commit of solution for bite 97

---
+++ 97/holidays.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+import os
+from urllib.request import urlretrieve
+
+from bs4 import BeautifulSoup
+
+
+# prep data
+tmp = os.getenv("TMP", "/tmp")
+page = "us_holidays.html"
+holidays_page = os.path.join(tmp, page)
+urlretrieve(f"https://bites-data.s3.us-east-2.amazonaws.com/{page}", holidays_page)
+
+with open(holidays_page) as f:
+    content = f.read()
+
+holidays = defaultdict(list)
+
+
+def get_us_bank_holidays(content=content):
+    """Receive scraped html output, make a BS object, parse the bank
+    holiday table (css class = list-table), and return a dict of
+    keys -> months and values -> list of bank holidays"""
+
+    holidays = defaultdict(list)
+
+    doc = BeautifulSoup(content, "html.parser")
+    rows = [
+        el
+        for el in doc("td")
+        if "style" in el.attrs and el["style"] == "white-space: nowrap"
+    ]
+    for holiday, datetime in [
+        (next(el.parent("a")[0].stripped_strings), el.parent("time")[0]["datetime"])
+        for el in rows
+    ]:
+        month = datetime[5:7]
+        holidays[month].append(holiday)
+
+    print(holidays)
+    return holidays
+
+
+get_us_bank_holidays()
