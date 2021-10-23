2d0fa230f1d071191d2f2e9fd763e1a6df6c1e73
Brian Dennis <bmd+github@crossjam.net>
2021-10-22 21:32:03 -0400

    Initial commit of solution to bite 150

---
+++ 150/jsonify.py
@@ -0,0 +1,24 @@
+import json
+import io
+import csv
+
+members = """
+id,first_name,last_name,email
+1,Junie,Kybert;jkybert0@army.mil
+2,Sid,Churching|schurching1@tumblr.com
+3,Cherry;Dudbridge,cdudbridge2@nifty.com
+4,Merrilee,Kleiser;mkleiser3@reference.com
+5,Umeko,Cray;ucray4@foxnews.com
+6,Jenifer,Dale|jdale@hubpages.com
+7,Deeanne;Gabbett,dgabbett6@ucoz.com
+8,Hymie,Valentin;hvalentin7@blogs.com
+9,Alphonso,Berwick|aberwick8@symantec.com
+10,Wyn;Serginson,wserginson9@naver.com
+"""
+
+
+def convert_to_json(members=members):
+    clean_members = members.replace("|", ",").replace(";", ",")
+    lines = clean_members.strip().splitlines()
+    keys = lines[0].split(",")
+    return json.dumps([dict(zip(keys, row.split(","))) for row in lines[1:]])
