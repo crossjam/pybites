04f67cb13316c504eae7e86306476b45a3ccf350
Brian Dennis <bmd+github@crossjam.net>
2021-10-29 18:39:05 -0400

    Remove comments

--- 274/conversion.py
+++ 274/conversion.py
@@ -4,10 +4,8 @@ def dec_to_base(number, base):
            base is the new base  (eg. 2, 6, or 8)
     Output: the converted number in the new base without the prefix (eg. '0b')
     """
-    # your code
 
     if number < base:
         return number
     else:
         return dec_to_base(number // base, base) * 10 + dec_to_base(number % base, base)
-    # return n
