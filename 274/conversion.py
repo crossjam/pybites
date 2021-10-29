914b19acadf75a39f99e612e543078ff52747e38
Brian Dennis <bmd+github@crossjam.net>
2021-10-29 18:37:26 -0400

    Initial commit of bite 274

---
+++ 274/conversion.py
@@ -0,0 +1,13 @@
+def dec_to_base(number, base):
+    """
+    Input: number is the number to be converted
+           base is the new base  (eg. 2, 6, or 8)
+    Output: the converted number in the new base without the prefix (eg. '0b')
+    """
+    # your code
+
+    if number < base:
+        return number
+    else:
+        return dec_to_base(number // base, base) * 10 + dec_to_base(number % base, base)
+    # return n
