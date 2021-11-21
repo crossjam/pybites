e5740bf1b2acef439053e634cfa55201ae4a0bc1
Brian Dennis <bmd+github@crossjam.net>
2021-11-20 20:56:02 -0500

    Initial solution to bite 63

---
+++ 63/README.md
@@ -0,0 +1,9 @@
+## [Bite 63. Use an infinite iterator to simulate a traffic light](https://codechalleng.es/bites/63/)
+
+Good luck and please share you code in the Bite forums upon completion.
+
+For questions use [our Slack](https://pybites.slack.com/archives/C6BGDQQ3B) (no spoilers please).
+
+Check out our full catalogue of Bites of Py [here](https://codechalleng.es/bites/catalogue).
+
+Enjoy and keep calm and code in Python!
---
+++ 63/traffic.py
@@ -0,0 +1,25 @@
+from collections import namedtuple
+from itertools import cycle, islice
+from time import sleep
+
+State = namedtuple("State", "color command timeout")
+
+
+def traffic_light():
+    """Returns an itertools.cycle iterator that
+    when iterated over returns State namedtuples
+    as shown in the Bite's description"""
+    states = [
+        State("red", "Stop", 2.0),
+        State("green", "Go", 2.0),
+        State("amber", "Caution", 0.5),
+    ]
+
+    return cycle(states)
+
+
+if __name__ == "__main__":
+    # print a sample of 10 states if run as standalone program
+    for state in islice(traffic_light(), 10):
+        print(f"{state.command}! The light is {state.color}")
+        sleep(state.timeout)
