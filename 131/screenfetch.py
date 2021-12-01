1ec3550ba93e7e51877e6b02137d175a247eb4bd
Brian Dennis <bmd+github@crossjam.net>
2021-11-30 22:43:23 -0500

    Initial commit of solution to bite 131

---
+++ 131/screenfetch.py
@@ -0,0 +1,47 @@
+import re
+from pprint import pprint
+
+MINT_OUTPUT = """
+                                       mohh@SERENiTY
+ MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        OS: Mint 19 tara
+ MMm----::-://////////////oymNMd+'     Kernel: x86_64 Linux 4.15.0-34-generic
+ MMd      /++                -sNMd:    Uptime: 1d 4m
+ MMNso/'  dMM    '.::-. .-::.' .hMN:   Packages: 2351
+ ddddMMh  dMM   :hNMNMNhNMNMNh: 'NMm   Shell: zsh 5.4.2
+     NMm  dMM  .NMN/-+MMM+-/NMN' dMM   Resolution: 1366x768
+     NMm  dMM  -MMm  'MMM   dMM. dMM   DE: Cinnamon 3.8.9
+     NMm  dMM  -MMm  'MMM   dMM. dMM   WM: Muffin
+     NMm  dMM  .mmd  'mmm   yMM. dMM   WM Theme: Linux Mint (Mint-Y)
+     NMm  dMM'  ..'   ...   ydm. dMM   GTK Theme: Mint-Y [GTK2/3]
+     hMM- +MMd/-------...-:sdds  dMM   Icon Theme: Mint-Y
+     -NMm- :hNMNNNmdddddddddy/'  dMM   Font: Noto Sans 9
+      -dMNs-''-::::-------.''    dMM   CPU: AMD A10-7400P Radeon R6, 10 Compute Cores 4C+6G @ 4x 2.5GHz [101.0°C]
+       '/dMNmy+/:-------------:/yMMM   GPU: AMD KAVERI (DRM 2.50.0 / 4.15.0-34-generic, LLVM 6.0.0)
+          ./ydNMMMMMMMMMMMMMMMMMMMMM   RAM: 1886MiB / 6915MiB
+             \.MMMMMMMMMMMMMMMMMMM    
+"""
+
+output = MINT_OUTPUT
+
+
+def sysinfo_scrape(output):
+    """Scrapes the output from screenfetch and returns a dictionary"""
+
+    info = {}
+
+    for line in output.splitlines():
+        m = re.search(r"((?:\s\w+)+): (.*)", line)
+
+        if m:
+            info[m.group(1).strip()] = m.group(2)
+            continue
+
+        m = re.search(r"   (\w+@\w+)", line)
+        if m:
+            info["Name"] = m.group(1)
+
+    return info
+
+
+if __name__ == "__main__":
+    pprint(sysinfo_scrape(MINT_OUTPUT))
